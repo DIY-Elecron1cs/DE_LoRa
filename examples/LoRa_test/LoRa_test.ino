82465baebd6293fd654b06d1a71df68a121ab892
DIY электроника <private.property240702@gmail.com>
2025-02-26 15:24:11 +0300

    Create LoRa_test.ino

---
+++ examples/LoRa_test/LoRa_test.ino
@@ -0,0 +1,27 @@
+#include <LoRa.h>      //сама библиотека
+
+/*---ПРИМЕР, использование---*/
+
+#define MODE 1  //0 - отправка, 1 - получение
+
+LoRa lora(2, 3);    //создаём объект класса LoRa
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+#if MODE == 1   //приём
+  int index, number;
+  if (lora.available()) {               //как в обычном Serial
+    lora.readCommand(index, number);    //читаем и записываем в переменные index и number
+
+    Serial.print(index);    //выводим
+    Serial.print('\t');
+    Serial.println(number);
+  }
+#else           //отправка
+  lora.sendCommand(5, 108);
+  delay(1000);
+#endif
+}
